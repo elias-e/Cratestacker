f6b03155020df4f1b6f10beda9ee6fc5a5bdf1a0
elias-e <elias.eckermann@student.kalksburg.at>
2020-06-18 18:11:02 +0200

    2. commit
    2. commit

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraManager : MonoBehaviour
+{
+    public float horizontalResolution = 1080;
+
+    void OnGUI()
+    {
+        float currentAspect = (float)Screen.width / (float)Screen.height;
+        Camera.main.orthographicSize = horizontalResolution / currentAspect / 200;
+
+    }
+}
